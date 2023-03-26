65d277991868375b7e5d6d250ccd48edd87f13be
RYGISA <127680491+RYGISA@users.noreply.github.com>
2023-03-26 18:55:38 +0300

    v2 pinpong
    python

--- pingpon.py
+++ pingpon.py
@@ -7,11 +7,12 @@ class GameSprite(sprite.Sprite):
         
 
         self.image = transform.scale(image.load(player_image), (wight, height))
-        self.speed = player_speedself.rect = self.image.get.rect()
+        self.speed = player_speed 
+        self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
     def reset(self):
-        window.blit(seslf.image, (self.rect.x, self.rect.y))
+        window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
@@ -33,7 +34,8 @@ background = transform.scale(image.load(back1),(win_width, win_height))
 
 game = True
 finish = False
-FPS = 144
+clock = time.Clock()
+FPS = 60
 racket1 = Player('16328137282623s.jpg', 30, 200, 4, 50, 150)
 racket2 = Player('16328137282623s.jpg', 520, 200, 4, 50, 150) 
 ball = GameSprite('620ea84ce3702bc204da3f512eb01461.png', 200, 200, 2, 50, 50)
@@ -43,10 +45,61 @@ lose1 = font.render('PLAYER 1 LOSE!', True,(180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True,(180, 0, 0))
 speed_x = 3
 speed_y = 3
+
+
+score1 = 0
+score2 = 0
+
+
+
+
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     if finish != True:
-        window_blit(background, (0, 0))
+        window.blit(background, (0, 0))
         racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        racket1.reset()
+        racket2.reset()
+        if sprite.collide_rect(racket1, ball):
+            speed_x *= -1
+            speed_y *= 1
+            score1 = score1 + 1
+        if sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+            score2 = score2 + 1
+
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+        text = font.render('Kilkict otbitux', 1, (0, 0, 255))
+        window.blit(text, (130, 20))
+        text_lose = font.render(str(score1) + " : " + str(score2), 1, (0, 0, 255))
+        window.blit(text_lose, (250, 60))
+        ball.reset()
+        display.update()
+    
+    else:
+        finisah = False
+        score2 = 0
+        score1 = 0
+        time.delay(3000)
+        ball = GameSprite('620ea84ce3702bc204da3f512eb01461.png',200, 200, 4, 50, 50)
+    clock.tick(FPS)
